6eac00555056b1781675377ef3bfdee4da1c5c72
Drageset <olve.drageset@gmail.com>
2018-02-03 17:21:58 +0100

    Made the timer work (counting down from a time variable)

--- src/GUI/SessionScreenController.java
+++ src/GUI/SessionScreenController.java
@@ -1,7 +1,15 @@
 package GUI;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * GUI Controller class for the session screen
@@ -10,6 +18,44 @@ public class SessionScreenController {
 
     static int width = CentralGUIControl.DEFAULT_WIDTH, height = CentralGUIControl.DEFAULT_HEIGHT;
     public Button backToMainButton;
+    public Label timeDisplay;
+    private boolean countingDown = false;
+    private int secondsLeft = 200;
+    private Timeline timeline;
+
+    private Timeline initializeTimer() {
+        timeline = new Timeline();
+        timeline.setCycleCount(secondsLeft);
+        KeyFrame frame = new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>(){
+
+            /**
+             * Invoked when a specific event of the type for which this handler is
+             * registered happens.
+             *
+             * @param event the event which occurred
+             */
+            @Override
+            public void handle(ActionEvent event) {
+                String minutes = Integer.toString(secondsLeft/60);
+                String seconds = Integer.toString(secondsLeft%60);
+                if (secondsLeft/60 < 10){
+                    minutes = "0" + minutes;
+                }
+                if (secondsLeft%60 < 10){
+                    seconds = "0" + seconds;
+                }
+                timeDisplay.setText(minutes + ":" + seconds);
+                secondsLeft--;
+                if (secondsLeft <= 0){
+                    timeline.stop();
+                }
+            }
+        });
+        timeline.getKeyFrames().add(frame);
+        return timeline;
+    }
+
+
 
     public void handleBackButtonClick(ActionEvent actionEvent) {
         Main.showStartScreen();
@@ -29,5 +75,18 @@ public class SessionScreenController {
 
     public static void setHeight(int height) {
         SessionScreenController.height = height;
+    }
+
+    public void handlePauseButtonClick(ActionEvent actionEvent) {
+        if (timeline == null){
+            initializeTimer();
+        }
+
+        if (countingDown){
+            timeline.stop();
+        } else {
+            timeline.play();
+        }
+        countingDown = !countingDown;
     }
 }
